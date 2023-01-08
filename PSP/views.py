354cd3774e56d73ee539d9a3319e0dcc6ceed6d8
김진호 <rlawlsgh1227@gmail.com>
2023-01-08 16:32:33 +0900

    feat: transfer id and pw to views.py using POST method

--- PSP/views.py
+++ PSP/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def index(request):
+    if request.method == "POST":
+        id_ = request.POST.get('id_')
+        pw = request.POST.get('password')
+
+        print(id_)
+        print(pw)
+
     return render(request, 'PSP/index.html')
