2494208220fae387c77633f0c49325ae592c583b
김진호 <rlawlsgh1227@gmail.com>
2023-01-17 23:55:32 +0900

    feat: Built a login system

--- PSP/models.py
+++ PSP/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.db.models.fields import BooleanField
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ['-created_at', '-updated_at']
+
+
+class CustomUser(AbstractUser, PermissionsMixin, TimestampedModel):
+    nickname = models.CharField(max_length=5)
+    is_active = BooleanField(default=True)
+    is_staff = BooleanField(default=False)
+
+    REQUIRED_FIELDS = ['nickname']
+
+    objects = CustomUserManager()
+
+    def get_full_name(self):
+        return self.nickname
+
+
