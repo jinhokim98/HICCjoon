5ec5eeea79c9b1bf004fe91fdfafbbcd8f441f5a
김진호 <rlawlsgh1227@gmail.com>
2023-01-21 17:52:25 +0900

    design: UI improvement signup.html

--- PSP/static/js/signup.js
+++ PSP/static/js/signup.js
@@ -1,7 +1,7 @@
-const join_name = document.getElementById("join_name");
-const join_id = document.getElementById("join_id");
-const join_pw = document.getElementById("join_pw");
-const join_check_pw = document.getElementById("join_check_pw");
+const join_name = document.querySelector('#join_name');
+const join_id = document.querySelector('#join_id');
+const join_pw = document.querySelector('#join_pw');
+const join_check_pw = document.querySelector('#join_confirm_pw');
 
 const join_row = {
     name: {
@@ -33,42 +33,42 @@ function valid_check() {
             show_nothing(join_row[key]['tag']);
         }
     }
-    
+
     is_difference_password();
 }
 
 function show_error(invalid) {
     if (invalid === join_name) {
-        document.getElementById('name_err_message').textContent = "2~5자의 한글을 사용하세요.";
+        document.querySelector('#name_err_message').innerHTML = "2~5자의 한글을 사용하세요.";
     } else if (invalid === join_id) {
-        document.getElementById('id_err_message').textContent = "4~16자의 영문, 숫자를 사용하세요.";
+        document.querySelector('#id_err_message').innerHTML = "4~16자의 영문, 숫자를 사용하세요.";
     } else if (invalid === join_pw) {
-        document.getElementById('pw_err_message').textContent = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
+        document.querySelector('#pw_err_message').innerHTML = "8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.";
     }
 }
 
 function show_nothing(valid) {
     if (valid === join_name) {
-        document.getElementById('name_err_message').textContent = '';
+        document.querySelector('#name_err_message').innerHTML = '';
     } else if (valid === join_id) {
-        document.getElementById('id_err_message').textContent = '';
+        document.querySelector('#id_err_message').innerHTML = '';
     } else if (valid === join_pw) {
-        document.getElementById('pw_err_message').textContent = '';
+        document.querySelector('#pw_err_message').innerHTML = '';
     }
 }
 
 function is_difference_password() {
     if (join_pw.value !== join_check_pw.value) {
-        document.getElementById('pw_check_err_message').textContent = "비밀번호가 같지 않습니다.";
+        document.querySelector('#pw_confirm_err_message').innerHTML = "비밀번호가 같지 않습니다.";
         return false;
 
     } else {
-        document.getElementById('pw_check_err_message').textContent = "";
+        document.querySelector('#pw_confirm_err_message').innerHTML = "";
         return true;
     }
 }
 
-document.getElementById("signup_btn").addEventListener('click', valid_check);
+document.querySelector('#signup_btn').addEventListener('click', valid_check);
 
 if (id_duplicate === "True") {
     alert("아이디 중복입니다.")
